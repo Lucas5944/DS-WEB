8c7b044b8a0319177adc1d1e1b74e3780e59cc5e
Lucas5944 <lucaus.ebaaaa@gmail.com>
2026-02-06 08:57:25 -0300

    adicionar arquivo do front

---
+++ folder/aula-01/script.js
@@ -0,0 +1,36 @@
+//var nome = ("lucas")
+//alert (nome)
+//var idade =  ("16")
+//alert (idade)
+let numero = Number ("25")
+console.log(numero)
+let numera = Number ("25")
+console.log("numero:"+numera)
+//var numero1 = 10
+//var numero2 = 50
+console.log(numero1 + numero2)
+window.alert("bem-vindo ao site")
+var resposta = window.confirm("deseja continuar?")
+console.log(resposta)
+let apagar = confirm("tem certeza  que deseja excluir?")
+if (apagar) {
+    alert("item apagado")
+  }  else{
+        alert("ação cancelada ")
+    }
+     var nome = prompt ("digite seu nome")
+     console.log(nome)
+     var numero1 = number(pronpt("digite o primeiro numero"))
+     var numero2 = number(pronpt("digite o segundo numero")) 
+     console.log(numero1 + nomero2) 
+      let nome = pronpt("qual e o seu nome")
+      if  (nome == null) {
+        alert("voce canselou" )
+      }else{
+            alert("ola,"+ nome +"!")
+        }
+      
+
+//let:variavel de escopo local
+//var:variavel de escopo global
+//const:variavel contante,nao pode ser alterada 
--- progamaçao-back-end/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<h1>pagina principal do meu servidor</h1>"
-?>
-<a href="/POO/">aula de POO</a>
