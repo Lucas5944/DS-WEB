5b2bb710e87597936540bb41500e96c5b9d1f162
Lucas5944 <lucaus.ebaaaa@gmail.com>
2026-02-27 13:40:14 -0300

    ronaldo

--- Front/aula-05/atividade-04/script.js
+++ Front/aula-05/atividade-04/script.js
@@ -1,34 +1,107 @@
-var area = document.getElementById("area")
-var mensagem = document.getElementById("mensagem")
+var pacman = document.getElementById("pacman");
+var comida = document.getElementById("comida");
+var placar = document.getElementById("placar");
+var fantasminha = document.getElementById("fantasminha");
 
+var X = window.innerWidth / 2;
+var Y = window.innerHeight / 2;
 
-area.addEventListener("click", function(){
-mensagem.textContent = "Clique simples detectado!";
- area.style.background = "white"
+var comidaX = 200;
+var comidaY = 200;
+var pontos = 0;
+
+let fantasminhaX = 100;
+let fantasminhaY = 100;
+let speed = 1;
+
+function moverFantasminha() {
+
+  if (fantasminhaX < X) {
+    fantasminhaX += speed;
+  } else if (fantasminhaX > X) {
+    fantasminhaX -= speed;
+  }
+
+  if (fantasminhaY < Y) {
+    fantasminhaY += speed;
+  } else if (fantasminhaY > Y) {
+    fantasminhaY -= speed;
+  }
+
+  fantasminha.style.marginLeft = fantasminhaX + "px";
+  fantasminha.style.marginTop = fantasminhaY + "px";
+}
+
+function verificarColisaoFantasminha() {
+
+  var distanciaX = Math.abs(X - fantasminhaX);
+  var distanciaY = Math.abs(Y - fantasminhaY);
+
+  if (distanciaX < 40 && distanciaY < 40) {
+    gameOver();
+  }
+}
+
+function gameOver() {
+  document.getElementById("tela-morte").style.display = "flex";
+}
+
+function reiniciar() {
+  location.reload();
+}
+
+function gameLoop() {
+  moverFantasminha();
+   verificarColisaoFantasminha()
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
+function desenhar() {
+  pacman.style.marginLeft = X + "px";
+  pacman.style.marginTop = Y + "px";
+
+  comida.style.marginLeft = comidaX + "px";
+  comida.style.marginTop = comidaY + "px";
+}
+
+document.addEventListener("keydown", function (event) {
+  var tecla = event.key.toLowerCase();
+  var velocidade = 20;
+
+  if (tecla == "w") {
+    Y -= velocidade;
+    pacman.style.transform = "rotate(-90deg)";
+  }
+  if (tecla == "s") {
+    Y += velocidade;
+    pacman.style.transform = "rotate(90deg)";
+  }
+  if (tecla == "a") {
+    X -= velocidade;
+    pacman.style.transform = "rotate(180deg)";
+  }
+  if (tecla == "d") {
+    X += velocidade;
+    pacman.style.transform = "rotate(0deg)";
+  }
+
+  verificarColisao();
+  desenhar();
 });
 
-area.addEventListener("dblclick", function(){
-        area.style.background  = "lightgreen";
-});
+function verificarColisao() {
 
-area.addEventListener("mouseenter", function(){
-mensagem.textContent = "O mouse entrou na área!";
-});
+  var distanciaX = Math.abs(X - comidaX);
+  var distanciaY = Math.abs(Y - comidaY);
 
-area.addEventListener("mouseleave", function(){
-mensagem.textContent = "O mouse saiu da área!";
-});
+  if (distanciaX < 40 && distanciaY < 40) {
+    pontos++;
+    placar.innerText = pontos;
 
-var posicao =document.getElementById("posicao")
-area.addEventListener("mousemove", function(event){
-posicao.textContent = "X:" + event.clientX + " Y:"
-+ event.clientY;
-});
-
-area.addEventListener("contextmenu", function(event){
-   
-  event.preventDefault();
-
-    alert("Botão direito clicado!");
-});
+    comidaX = Math.floor(Math.random() * (window.innerWidth - 50));
+    comidaY = Math.floor(Math.random() * (window.innerHeight - 100)) + 50;
+  }
+}
 
